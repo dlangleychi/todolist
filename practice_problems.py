b09092d95e2c03516d733f10b36613e47ae4923b
Daniel Langley <dlangley812@hotmail.com>
2026-01-22 13:59:00 -0600

    Define Todolist and do practice problems

---
+++ practice_problems.py
@@ -0,0 +1,73 @@
+# #  1
+
+# def select(callback, iterable):
+#     return [item for item in iterable if callback(item)]
+
+# numbers = [1, 2, 3, 4, 5]
+# colors = {'red', 'orange', 'yellow', 'green',
+#           'blue', 'indigo', 'violet'}
+
+# odd_numbers = select(lambda number: number % 2 != 0, numbers)
+# print(odd_numbers)            # [1, 3, 5]
+
+# large_numbers = select(lambda number: number >= 10, numbers)
+# print(large_numbers)          # []
+
+# small_numbers = select(lambda number: number < 10, numbers)
+# print(small_numbers)          # [1, 2, 3, 4, 5]
+
+# short_color_names = select(lambda color: len(color) <= 5, colors)
+# print(short_color_names)      # ['blue', 'red', 'green']
+# # The order of the colors may vary, but should include the
+# # indicated colors.
+
+# def reject(callback, iterable):
+#     return [item for item in iterable if not callback(item)]
+
+# numbers = [1, 2, 3, 4, 5]
+# colors = {'red', 'orange', 'yellow', 'green',
+#           'blue', 'indigo', 'violet'}
+
+# even_numbers = reject(lambda number: number % 2 != 0, numbers)
+# print(even_numbers)            # [2, 4]
+
+# small_numbers = reject(lambda number: number >= 10, numbers)
+# print(small_numbers)          # [1, 2, 3, 4, 5]
+
+# large_numbers = reject(lambda number: number < 10, numbers)
+# print(large_numbers)          # []
+
+# long_color_names = reject(lambda color: len(color) <= 5, colors)
+# print(long_color_names)
+# # ['yellow', 'violet', 'orange', 'indigo']
+# # The order of the colors may vary, but should include the
+# # indicated colors.
+
+def reduce(callback, iterable, starting_value):
+    accum = starting_value
+    for item in iterable:
+        accum = callback(item, accum)
+    return accum
+
+# numbers = (1, 2, 4, 8, 16)
+# total = lambda number, accum: accum + number
+# print(reduce(total, numbers, 0))        # 31
+
+# numbers = [10, 3, 5]
+# product = lambda number, accum: accum * number
+# print(reduce(product, numbers, 2))      # 300
+
+# colors = ['red', 'orange', 'yellow', 'green',
+#           'blue', 'indigo', 'violet']
+# rainbow = lambda color, accum: accum + color[0].upper()
+# print(reduce(rainbow, colors, ''))      # ROYGBIV
+
+numbers = list(range(1, 6))
+
+numbers = [3, 7, 2, 9, 5]
+
+add_square = lambda num, accum : accum + num ** 2
+
+sum_squares = reduce(add_square, numbers, 0)
+
+print(sum_squares) # 55
---
+++ todolist.py
@@ -0,0 +1,357 @@
+class Todo:
+
+    COMPLETE = 'X'
+    INCOMPLETE = ' '
+
+    def __init__(self, title):
+        self._title = title
+        self.done = False
+
+    @property
+    def title(self):
+        return self._title
+    
+    @property
+    def done(self):
+        return self._done
+    
+    @done.setter
+    def done(self, value):
+        self._done = value
+
+    def __str__(self):
+        mark = Todo.COMPLETE if self.done else Todo.INCOMPLETE
+        return f'[{mark}] {self.title}'
+    
+    def __eq__(self, other):
+        if not isinstance(other, Todo):
+            return NotImplemented
+        
+        return self.title == other.title and self.done == other.done
+
+class TodoList:
+    def __init__(self, title):
+        self._title = title
+        self._todos = []
+
+    @property
+    def title(self):
+        return self._title
+    
+    def add(self, todo):
+        if not isinstance(todo, Todo):
+            raise TypeError('Added object must be type Todo.')
+        
+        self._todos.append(todo)
+
+    def __str__(self):
+        return f"----- {self._title} -----\n{'\n'.join(map(str, self._todos))}"
+
+    def __len__(self):
+        return len(self._todos)
+    
+    def first(self):
+        return self._todos[0]
+    
+    def last(self):
+        return self._todos[-1] 
+
+    def to_list(self):
+        return list(self._todos)
+
+    def todo_at(self, index):
+        return self._todos[index]
+    
+    def mark_done_at(self, index):
+        self.todo_at(index).done = True
+
+    def mark_undone_at(self, index):
+        self.todo_at(index).done = False
+
+    def mark_all_done(self):
+        for todo in self._todos:
+            todo.done = True
+
+    def mark_all_undone(self):
+        for todo in self._todos:
+            todo.done = False
+
+    def all_done(self):
+        return all(todo.done for todo in self._todos)
+
+    def remove_at(self, index):
+        self._todos.pop(index)
+
+def test_todo():
+    todo1 = Todo('Buy milk')
+    todo2 = Todo('Clean room')
+    todo3 = Todo('Go to gym')
+    todo4 = Todo('Clean room')
+
+    print(todo1)                  # [ ] Buy milk
+    print(todo2)                  # [ ] Clean room
+    print(todo3)                  # [ ] Go to gym
+    print(todo4)                  # [ ] Clean room
+
+    print(todo2 == todo4)         # True
+    print(todo1 == todo2)         # False
+    print(todo4.done)             # False
+
+    todo1.done = True
+    todo4.done = True
+    print(todo4.done)             # True
+
+    print(todo1)                  # [X] Buy milk
+    print(todo2)                  # [ ] Clean room
+    print(todo3)                  # [ ] Go to gym
+    print(todo4)                  # [X] Clean room
+
+    print(todo2 == todo4)         # False
+
+    todo4.done = False
+    print(todo4.done)             # False
+    print(todo4)                  # [ ] Clean room
+
+# test_todo()
+
+# Code omitted for brevity.
+
+empty_todo_list = TodoList('Nothing Doing')
+
+def setup():
+    todo1 = Todo('Buy milk')
+    todo2 = Todo('Clean room')
+    todo3 = Todo('Go to gym')
+
+    todo2.done = True
+
+    todo_list = TodoList("Today's Todos")
+    todo_list.add(todo1)
+    todo_list.add(todo2)
+    todo_list.add(todo3)
+
+    return todo_list
+
+# Code omitted
+
+def step_1():
+    print('--------------------------------- Step 1')
+    todo_list = setup()
+
+    # setup() uses `todo_list.add` to add 3 todos
+
+    try:
+        todo_list.add(1)
+    except TypeError:
+        print('TypeError detected')    # TypeError detected
+
+    for todo in todo_list._todos:
+        print(todo)
+
+step_1()
+
+def step_2():
+    print('--------------------------------- Step 2')
+    todo_list = setup()
+
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [X] Clean room
+    # [ ] Go to gym
+
+step_2()
+
+def step_3():
+    print('--------------------------------- Step 3')
+    todo_list = setup()
+
+    print(len(todo_list))              # 3
+    print(len(empty_todo_list))        # 0
+
+step_3()
+
+def step_4():
+    print('--------------------------------- Step 4')
+    todo_list = setup()
+
+    print(todo_list.first())           # [ ] Buy milk
+    print(todo_list.last())            # [ ] Go to gym
+
+    try:
+        empty_todo_list.first()
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    try:
+        empty_todo_list.last()
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+step_4()
+
+def step_5():
+    print('--------------------------------- Step 5')
+    todo_list = setup()
+
+    print(empty_todo_list.to_list())    # []
+
+    todos = todo_list.to_list()
+    print(type(todos).__name__)         # list
+
+    for todo in todos:
+        print(todo)                     # [ ] Buy milk
+                                        # [X] Clean room
+                                        # [ ] Go to gym
+
+step_5()
+
+def step_6():
+    print('--------------------------------- Step 6')
+    todo_list = setup()
+
+    print(todo_list.todo_at(0))        # [ ] Buy milk
+    print(todo_list.todo_at(1))        # [X] Clean room
+    print(todo_list.todo_at(2))        # [ ] Go to gym
+
+    try:
+        todo_list.todo_at(3)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    # Ensure we have a reference
+    print(todo_list.todo_at(1) is todo_list.todo_at(1))  # True
+
+step_6()
+
+def step_7():
+    print('--------------------------------- Step 7')
+    todo_list = setup()
+
+    todo_list.mark_done_at(0)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room
+    # [ ] Go to gym
+
+    todo_list.mark_done_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room
+    # [ ] Go to gym
+
+    todo_list.mark_done_at(2)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room
+    # [X] Go to gym
+
+    try:
+        todo_list.mark_done_at(3)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    todo_list.mark_undone_at(0)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [X] Clean room
+    # [X] Go to gym
+
+    todo_list.mark_undone_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Clean room
+    # [X] Go to gym
+
+    todo_list.mark_undone_at(2)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Clean room
+    # [ ] Go to gym
+
+    try:
+        todo_list.mark_undone_at(3)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+step_7()
+
+def step_8():
+    print('--------------------------------- Step 8')
+    todo_list = setup()
+
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [X] Clean room
+    # [ ] Go to gym
+
+    todo_list.mark_all_done()
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [X] Buy milk
+    # [X] Clean room
+    # [X] Go to gym
+
+    todo_list.mark_all_undone()
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Clean room
+    # [ ] Go to gym
+
+step_8()
+
+def step_9():
+    print('--------------------------------- Step 9')
+    todo_list = setup()
+
+    print(todo_list.all_done())         # False
+
+    todo_list.mark_all_done()
+    print(todo_list.all_done())         # True
+
+    todo_list.mark_undone_at(1)
+    print(todo_list.all_done())         # False
+
+    print(empty_todo_list.all_done())   # True
+
+step_9()
+
+def step_10():
+    print('--------------------------------- Step 10')
+    todo_list = setup()
+
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [X] Clean room
+    # [ ] Go to gym
+
+    todo_list.remove_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+    # [ ] Go to gym
+
+    todo_list.remove_at(1)
+    print(todo_list)
+    # ---- Today's Todos -----
+    # [ ] Buy milk
+
+    try:
+        todo_list.remove_at(1)
+    except IndexError:
+        print('Expected IndexError: Got it!')
+
+    todo_list.remove_at(0)
+    print(todo_list)
+    # ---- Today's Todos -----
+
+step_10()
